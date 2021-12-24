9e6d1f9907ecaa4e83f01f8d789a98934ca3b182
Quiet-Clicking-Sounds <55921097+Quiet-Clicking-Sounds@users.noreply.github.com>
2021-12-24 17:58:39 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf8") as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name="item_network-pkg-Quiet_Clicking_Sounds",
+    version="0.0.1",
+    author="Quiet_Clicking_Sounds",
+    description="Linked network package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Quiet-Clicking-Sounds/item_network",
+    project_urls={"Bug Tracker": "https://github.com/Quiet-Clicking-Sounds/item_network/issues"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: The Unlicense (Unlicense)",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+)
