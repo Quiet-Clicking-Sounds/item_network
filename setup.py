b65fe6d9e6d904ee748f3425f6b94982797d9bee
Quiet-Clicking-Sounds <55921097+Quiet-Clicking-Sounds@users.noreply.github.com>
2021-12-28 13:02:44 +0000

    initial

--- setup.py
+++ setup.py
@@ -13,11 +13,13 @@ setuptools.setup(
     url="https://github.com/Quiet-Clicking-Sounds/item_network",
     project_urls={"Bug Tracker": "https://github.com/Quiet-Clicking-Sounds/item_network/issues"},
     classifiers=[
+        'Development Status :: 4 - Beta',
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: The Unlicense (Unlicense)",
         "Operating System :: OS Independent",
     ],
     package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.7",
+    packages=setuptools.find_packages(where="src/itemnetwork"),
+    python_requires=">=3.8",
+    extras_require=setuptools.find_packages(where="src/visualization"),
 )
